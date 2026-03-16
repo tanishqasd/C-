f0ad450df2d2d86ab346eada99d1de7424d35cf7
tanishqasd <tanishqadayma034@gmail.com>
2026-03-16 14:49:16 +0530

    Added program to find largest of three numbers

---
+++ level 1/threelargestnos9.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1, num2, num3;
+
+        Console.Write("Enter first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        num3 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 >= num2 && num1 >= num3)
+        {
+            Console.WriteLine("Largest number is: " + num1);
+        }
+        else if (num2 >= num1 && num2 >= num3)
+        {
+            Console.WriteLine("Largest number is: " + num2);
+        }
+        else
+        {
+            Console.WriteLine("Largest number is: " + num3);
+        }
+    }
+}
